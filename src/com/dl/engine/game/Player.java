82843983a4755101c87f14a4a21e19dad1261074
Luiz Henrique <luizlink64@gmail.com>
2021-01-02 11:11:24 -0300

    Improvment on player hitbox

--- src/com/dl/engine/game/Player.java
+++ src/com/dl/engine/game/Player.java
@@ -11,34 +11,37 @@ import com.dl.engine.gfx.ImageTile;
 import controller.assets.assetsController;
 import java.awt.event.KeyEvent;
 
-
 /**
  *
  * @author Luiz
  */
 public class Player extends GameObject
 {
+
     private static final int RIGHT = 0;
     private static final int LEFT = 1;
-    
+
     private ImageTile playerImage = new ImageTile(assetsController.CHAR_SPRITES_DIR + "playerSprites.png", 16, 16);
-    
+
+    private int padding;
+    private int paddingTop;
+
     private int direction = RIGHT;
     private float animationFrame = 0;
-    
+
     private int tileX;
     private int tileY;
     private float offX;
     private float offY;
-    
+
     private float speed = 100;
     private float fallSpeed = 10;
     private float jump = -4;
     private boolean ground = false;
     private boolean groundLast = false;
-    
+
     private float fallDistance = 0;
-    
+
     public Player(int posX, int posY)
     {
         this.tag = "player";
@@ -50,6 +53,8 @@ public class Player extends GameObject
         this.posY = posY * GameManager.TILE_SIZE;
         this.width = GameManager.TILE_SIZE;
         this.height = GameManager.TILE_SIZE;
+        this.padding = 5;
+        this.paddingTop = 2;
     }
 
     @Override
@@ -57,137 +62,122 @@ public class Player extends GameObject
     {
         //Andar para esquerda e direita
         //direita
-        if(ge.getInput().isKey(KeyEvent.VK_D))
+        if (ge.getInput().isKey(KeyEvent.VK_D))
         {
-            if (gm.getCollision(tileX +1, tileY) || gm.getCollision(tileX +1 , tileY + (int)Math.signum((int)offY)))
+            if (gm.getCollision(tileX + 1, tileY) || gm.getCollision(tileX + 1, tileY + (int) Math.signum((int) offY)))
             {
-                if (offX < 0)
+                offX += deltaTime * speed;
+                if (offX > padding)
                 {
-                    offX += deltaTime * speed;
-                    if (offX > 0)
-                    {
-                        offX = 0;
-                    }
+                    offX = padding;
                 }
-                else {
-                    offX = 0;
-                }
-            }
-            else {
+            } else
+            {
                 offX += deltaTime * speed;
             }
         }
         //esquerda
-        if(ge.getInput().isKey(KeyEvent.VK_A))
+        if (ge.getInput().isKey(KeyEvent.VK_A))
         {
-            if (gm.getCollision(tileX -1, tileY) || gm.getCollision(tileX -1 , tileY + (int)Math.signum((int)offY)))
+            if (gm.getCollision(tileX - 1, tileY) || gm.getCollision(tileX - 1, tileY + (int) Math.signum((int) offY)))
             {
-                if (offX > 0)
+                offX -= deltaTime * speed;
+                if (offX < -padding)
                 {
-                    offX -= deltaTime * speed;
-                    if (offX < 0)
-                    {
-                        offX = 0;
-                    }
+                    offX = -padding;
                 }
-                else {
-                    offX = 0;
-                }
-            }
-            else {
+            } else
+            {
                 offX -= deltaTime * speed;
             }
         }
-        
+
         //Fim do movimento lateral
-        
         //Começo do pulo e gravidade
         fallDistance += deltaTime * fallSpeed;
-        
-        if(ge.getInput().isKeyDown(KeyEvent.VK_W) && ground)
+
+        if (ge.getInput().isKeyDown(KeyEvent.VK_W) && ground)
         {
             fallDistance = jump;
             ground = false;
         }
-        
+
         offY += fallDistance;
-        
+
         if (fallDistance < 0)
         {
-            if((gm.getCollision(tileX, tileY - 1) || gm.getCollision(tileX + (int)Math.signum((int)offX), tileY - 1)) && offY < 0)
+            if ((gm.getCollision(tileX, tileY - 1) || gm.getCollision(tileX + (int) Math.signum((int) Math.abs(offX) > padding ? offX : 0), tileY - 1)) 
+                    && offY < -paddingTop)
             {
                 fallDistance = 0;
-                offY = 0;
+                offY = -paddingTop;
             }
         }
-        
+
         if (fallDistance > 0)
         {
-            
             ground = false;
-            if((gm.getCollision(tileX, tileY + 1) || gm.getCollision(tileX + (int) Math.signum((int)offX), tileY + 1)) && offY > 0)
+            if ((gm.getCollision(tileX, tileY + 1) || gm.getCollision(tileX + (int) Math.signum((int) Math.abs(offX) > padding ? offX : 0), tileY + 1)) 
+                    && offY > 0)
             {
                 fallDistance = 0;
                 offY = 0;
                 ground = true;
             }
         }
-        
+
         //Fim do pulo e gravidade
-        
-        
         //Posição Final
-        if(offY > GameManager.TILE_SIZE / 2)
+        if (offY > GameManager.TILE_SIZE / 2)
         {
             tileY++;
             offY -= GameManager.TILE_SIZE;
         }
-        
-        if(offY < -GameManager.TILE_SIZE / 2)
+
+        if (offY < -GameManager.TILE_SIZE / 2)
         {
             tileY--;
             offY += GameManager.TILE_SIZE;
         }
-        
-        if(offX > GameManager.TILE_SIZE / 2)
+
+        if (offX > GameManager.TILE_SIZE / 2)
         {
             tileX++;
             offX -= GameManager.TILE_SIZE;
         }
-        
-        if(offX < -GameManager.TILE_SIZE / 2)
+
+        if (offX < -GameManager.TILE_SIZE / 2)
         {
             tileX--;
             offX += GameManager.TILE_SIZE;
         }
-        
+
         posX = tileX * GameManager.TILE_SIZE + offX;
         posY = tileY * GameManager.TILE_SIZE + offY;
-        
-        
+
         //Verifica se apertou botao para atirar projetil
-        if(ge.getInput().isKey(KeyEvent.VK_UP))
+        if (ge.getInput().isKey(KeyEvent.VK_UP))
         {
             gm.addObject(new Bullet(tileX, tileY, offX + width / 2, offY + height / 2, 0));
         }
-        
-        if(ge.getInput().isKey(KeyEvent.VK_RIGHT))
+
+        if (ge.getInput().isKey(KeyEvent.VK_RIGHT))
         {
             gm.addObject(new Bullet(tileX, tileY, offX + width / 2, offY + height / 2, 1));
         }
-        
-        if(ge.getInput().isKey(KeyEvent.VK_DOWN))
+
+        if (ge.getInput().isKey(KeyEvent.VK_DOWN))
         {
             gm.addObject(new Bullet(tileX, tileY, offX + width / 2, offY + height / 2, 2));
         }
-        
-        if(ge.getInput().isKey(KeyEvent.VK_LEFT))
+
+        if (ge.getInput().isKey(KeyEvent.VK_LEFT))
         {
             gm.addObject(new Bullet(tileX, tileY, offX + width / 2, offY + height / 2, 3));
         }
-        
+
         //Check which direction character should face
-        if(ge.getInput().isKey(KeyEvent.VK_D))
+        if (ge.getInput().isKey(KeyEvent.VK_D))
         {
             direction = RIGHT;
             animationFrame += deltaTime * 8;
@@ -195,8 +185,7 @@ public class Player extends GameObject
             {
                 animationFrame = 0;
             }
-        }
-        else if(ge.getInput().isKey(KeyEvent.VK_A))
+        } else if (ge.getInput().isKey(KeyEvent.VK_A))
         {
             direction = LEFT;
             animationFrame += deltaTime * 8;
@@ -204,30 +193,29 @@ public class Player extends GameObject
             {
                 animationFrame = 0;
             }
-        }
-        else
+        } else
         {
             animationFrame = 0;
         }
-        
-        if(!ground)
+
+        if (!ground)
         {
             animationFrame = 1;
         }
-        
-        if(ground && !groundLast)
+
+        if (ground && !groundLast)
         {
             animationFrame = 2;
         }
-        
+
         groundLast = ground;
     }
 
     @Override
     public void render(GameEngine ge, Renderer r)
     {
-        r.drawImageTile(playerImage, (int)posX, (int)posY, (int)animationFrame, direction);
+        r.drawImageTile(playerImage, (int) posX, (int) posY, (int) animationFrame, direction);
         //r.drawFillRect((int)posX, (int)posY, width, height, 0xff00ff00);
     }
-    
+
 }
